ab0460b4fcfbb08f644e1f1d268f1efc37974d45
Cosimo Inserra <cinserra@users.noreply.github.com>
2016-03-31 17:35:51 +0100

    Added files via upload

---
+++ tools/parse_out_email_text.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+'''
+Created by Cosimo Inserra
+github: https://github.com/cinserra
+Twitter: @COSMO_83
+
+Updated tools for the udacity machine-learnign final project on the Enron dataset
+'''
+
+from nltk.stem.snowball import SnowballStemmer
+import string
+
+def parseOutText(f):
+    """ given an opened email file f, parse out all text below the
+        metadata block at the top
+        (in Part 2, you will also add stemming capabilities)
+        and return a string that contains all the words
+        in the email (space-separated)
+
+        example use case:
+        f = open("email_file_name.txt", "r")
+        text = parseOutText(f)
+
+        """
+
+
+    f.seek(0)  ### go back to beginning of file
+    all_text = f.read()
+
+    ### split off metadata
+    content = all_text.split("X-FileName:")
+    words = ""
+    if len(content) > 1:
+        ### remove punctuation
+        text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
+
+        ### split the text string into individual words, stem each word,
+        ### and append the stemmed word to words
+        stemmer = SnowballStemmer("english")
+        split_words = text_string.split()
+        word_list =[]
+        for word in split_words:
+            word_list.append(stemmer.stem(word))
+            #words += str(" ")+str(stemmer.stem(word)) #alternative version
+
+        #words = stemmer.stem(words)
+        words = ' '.join(word_list)
+
+    return words
+
+
+
+def main():
+    ff = open("../text_learning/test_email.txt", "r")
+    text = parseOutText(ff)
+    print text
+
+
+
+if __name__ == '__main__':
+    main()
